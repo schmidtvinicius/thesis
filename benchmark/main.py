c0edfd807a7169514ae5d71a78627c20d1cc6908
Vinicius Ribeiro Machado Schmidt <vinimachado.s@protonmail.com>
2026-01-09 16:50:55 +0100

    Removed some command line arguments and changed `Thread` to `Process`

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,20 +1,15 @@
 import argparse
 import asyncio
-import benchmark.dataset as dataset
-import duckdb
-import json
 import os
+from types import LambdaType
 import pyarrow as pa
-import pyarrow.dataset as ds
-import pyarrow.parquet as pq
 import threading
+import time
 
-from benchmark.dataset import ALLOWED_FORMATS, create_dataset, Dataset
+from benchmark.dataset import Dataset
 from benchmark.kafka_interface import KafkaInterface
-from confluent_kafka import KafkaError, Consumer
-from confluent_kafka.admin import AdminClient
 from dotenv import load_dotenv
-from time import sleep
+from multiprocessing import Process
 
 # This needs to happens before we import pyiceberg, otherwise, it doesn't know about the variables in the .env file
 load_dotenv()
@@ -22,7 +17,6 @@ load_dotenv()
 from benchmark.arrow_interface.arrow_interface import ArrowInterface
 from benchmark.arrow_interface.ducklake_interface import DuckLakeInterface
 from benchmark.arrow_interface.iceberg_interface import IcebergInterface
-from pyarrow.csv import read_csv, ReadOptions, ParseOptions, ConvertOptions
 
 TABLE_SCHEMA = pa.schema([
     pa.field("id", pa.string(), nullable=False),
@@ -35,11 +29,11 @@ TABLE_SCHEMA = pa.schema([
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", help="The path to a dataset")
-    parser.add_argument("--format", choices=ALLOWED_FORMATS, required=False)
+    # parser.add_argument("--dataset", help="The path to a dataset")
+    # parser.add_argument("--format", choices=ALLOWED_FORMATS, required=False,)
     parser.add_argument("--write-batch-size", type=int, default=1)
-    # parser.add_argument("--scale-factor", type=int, help="The scale factor to generate data at.")
-    parser.add_argument("--lakehouse", choices=["iceberg", "delta", "ducklake", "all"], default="all")
+    # parser.add_argument("--total-events", type=int, help="The number of events to generate")
+    parser.add_argument("--lakehouse", choices=["iceberg", "delta", "ducklake", "all"], default="ducklake")
     parser.add_argument("--client", choices=["native", "duckdb"], default="native", help="The Python client used to interact with each table format.")
     return parser.parse_args()
         
@@ -51,23 +45,36 @@ async def run_experiment(
         dataset: Dataset,
         total_events: int, 
         write_batch_size: int = 1):
-    
-    for lakehouse in lakehouses:
+            
+    experiments = {"experiment": [], "dataset": []}
+    total_start_time = time.perf_counter()
+    for i, lakehouse in enumerate(lakehouses):
+        experiments["experiment"].append(f"Experiment_{i}")
         await kafka_interface.create_topic(topic)
-        t1 = threading.Thread(target=kafka_interface.produce, args=[topic, dataset, total_events])
-        t1.start()
+        stop_event = threading.Event()
+        p1 = Process(target=kafka_interface.produce, args=(topic, dataset, total_events))
+        p1.start()
         kafka_interface.consume_and_write(topic, lakehouse, total_events, dataset.schema, write_batch_size)
-        t1.join()
+        p1.join()
         await kafka_interface.delete_topic(topic)
         # lakehouse.write_to_table(table)
+    total_end_time = time.perf_counter()
 
 
 async def main():
-    total_events = 1_000_000
+    from confluent_kafka import Producer
+    from datetime import datetime
+    import json
+    total_events = 100_000
     args = get_args()
-    dataset = create_dataset(args.dataset, args.format)
-    # kafka_admin = AdminClient(conf={"bootstrap.servers": os.getenv("KAFKA_BOOTSTRAP_SERVERS")})
+    dataset = Dataset()
     kafka_interface = KafkaInterface(os.getenv("KAFKA_BOOTSTRAP_SERVERS"))
+
+    # start_time = datetime.now()
+    # kafka_interface.produce(os.getenv("KAFKA_TOPIC"), dataset, total_events, threading.Event())
+    # end_time = datetime.now()
+    # print(f"Took {(end_time - start_time).total_seconds()} seconds to produce {total_events} events")
+    # return
         
     if args.lakehouse == "all":
         lakehouses = [IcebergInterface(table_schema=dataset.schema), DuckLakeInterface(dataset.schema)]
@@ -77,6 +84,7 @@ async def main():
         lakehouses = [DuckLakeInterface(table_schema=dataset.schema)]
     else:
         raise NotImplementedError
+    experiment_stats = vars(args)
     await run_experiment(lakehouses, kafka_interface, os.getenv("KAFKA_TOPIC"), dataset, total_events, args.write_batch_size)
 
 
