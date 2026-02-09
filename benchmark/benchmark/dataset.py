6e574f93aa73b33654f4b144672e81261dc8c1fa
Vinicius Ribeiro Machado Schmidt <vinimachado.s@protonmail.com>
2026-02-09 09:07:09 +0100

    Added explicit Arrow schema and utility method for retrieve Arrow IPC data

--- benchmark/benchmark/dataset.py
+++ benchmark/benchmark/dataset.py
@@ -1,53 +1,50 @@
 import duckdb
-import os
+import pyarrow as pa
 
 from datetime import datetime
 
-ALLOWED_FORMATS = ["csv", "parquet"]
+
+SCHEMA = pa.schema([
+    ("VendorID", pa.int32()),
+    ("tpep_pickup_datetime", pa.timestamp("us")),
+    ("tpep_dropoff_datetime", pa.timestamp("us")),
+    ("passenger_count", pa.int64()),
+    ("trip_distance", pa.float64()),
+    ("RatecodeID", pa.int64()),
+    ("store_and_fwd_flag", pa.string()),
+    ("PULocationID", pa.int32()),
+    ("DOLocationID", pa.int32()),
+    ("payment_type", pa.int64()),
+    ("fare_amount", pa.float64()),
+    ("extra", pa.float64()),
+    ("mta_tax", pa.float64()),
+    ("tip_amount", pa.float64()),
+    ("tolls_amount", pa.float64()),
+    ("improvement_surcharge", pa.float64()),
+    ("total_amount", pa.float64()),
+    ("congestion_surcharge", pa.float64()),
+    ("Airport_fee", pa.float64()),
+    ("cbd_congestion_fee", pa.float64()),
+    ("trip_id", pa.int64())
+])
+
 
 class Dataset():
 
-    def __init__(self):
-        self.data = duckdb.read_csv(os.getenv("DATASET_PATH"))
-        self.schema = self.data.arrow().schema
+    def __init__(self, path):
+        self.data = duckdb.read_parquet(path).arrow(1)
 
-    def get_next_event(self) -> dict:
-        return {k:(datetime.isoformat(v, sep=" ") if isinstance(v, datetime) else v)  for k,v in zip(self.data.columns, self.data.fetchone())}
+    
+    def __del__(self):
+        self.data.close()
+
+
+    # def get_next_event(self) -> dict:
+    #     return {k:(datetime.isoformat(v, sep=" ") if isinstance(v, datetime) else v)  for k,v in zip(self.data.columns, self.data.fetchone())}
     
 
-# class NYCTaxiDataset():
-#     pass
-    
-
-# class CSVDataset(Dataset):
-    
-#     def __init__(self, source: str):
-#         super().__init__()
-#         self.data = duckdb.read_csv(source)
-#         self.schema = self.data.arrow().schema
-
-
-# class ParquetDataset(Dataset):
-
-#     def __init__(self, source: str):
-#         super().__init__()
-#         self.data = duckdb.read_parquet(source)
-#         self.schema = self.data.arrow().schema
-
-
-# class TPCHDataset(Dataset):
-
-#     def __init__(self, sf: int = 1):
-#         raise NotImplementedError()
-
-
-
-# def create_dataset(source: str, format: str) -> Dataset:
-#     if source.lower() == "tpch":
-#         return TPCHDataset()
-#     if format.lower() is None or format.lower() not in ALLOWED_FORMATS:
-#         raise ValueError(f"When `source` is not 'tpch', `format` must be specified to one of {ALLOWED_FORMATS}")
-#     if format.lower() == "csv":
-#         return CSVDataset(source=source)
-#     if format.lower() == "parquet":
-#         return ParquetDataset(source=source)
+    def get_next_batch(self):
+        sink = pa.BufferOutputStream()
+        with pa.ipc.new_stream(sink, SCHEMA) as writer:
+            writer.write_batch(self.data.read_next_batch())
+        return sink.getvalue()
